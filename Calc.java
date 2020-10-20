bad5d843e00827581ea6f82ca2aeb0ac2dba906b
ShivamPali-2020 <67118709+ShivamPali-2020@users.noreply.github.com>
2020-10-20 11:18:42 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -12,7 +12,7 @@ public static int prod(int a,int b){
   public static void main(String []args){
     System.out.println(add(10,20));
     System.out.println(sub(30,10));
-    System.out.println(sub(20,10));
+    System.out.println(prod(20,10));
 
  }
 }
